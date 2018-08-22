9b5882f1641203b3cd9fafa229e371aade1d89d8
Romain TALDU <r.taldu@agglo-pau.fr>
2018-08-22 14:18:39 +0200

    modification des tables user et teams

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,11 +23,6 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->tinyInteger('role')->default('1')->comment('1 => User, 2 => Admin, 3 => Super-Admin');
             $table->boolean('state')->default('0');// utilsateur validé ou non
-            $table->tinyInteger('subscription')->default('1')->comment('1 => Basic, 2 => Standard, 3 => Premium');
-            
-            $dateMore1year=date("Y-m-d", strtotime("+1 year"));// Date + 1 an
-
-            $table->date('endofsubscription')->default($dateMore1year);
             $table->string('team_id')->default('0');// clé étrangere
             $table->foreign('team_id')->references('id')->on('team')->onDelete('cascade');// verifier pertinence cascade ?
             $table->rememberToken();
@@ -36,7 +31,7 @@ class CreateUsersTable extends Migration
 
          // Insertion utisateurs initiaux
          DB::table('users')->insert(
-        ['lastName' => 'TALDU','firstName' => 'Romain', 'email' => 'romain.taldu@gmail.com', 'password' => 'anonymous', 'role' => '3', 'state' => '1','subscription' => '3','endofsubscription' => '2100-12-31']
+        ['lastName' => 'TALDU','firstName' => 'Romain', 'email' => 'romain.taldu@gmail.com', 'password' => 'anonymous', 'role' => '3', 'state' => '1']
     ); 
 
     }
--- database/migrations/2018_08_19_113749_create_teams_tables.php
+++ database/migrations/2018_08_19_113749_create_teams_tables.php
@@ -14,15 +14,25 @@ class CreateTeamsTables extends Migration
     public function up()
     {
         Schema::create('teams', function (Blueprint $table) {
+
+            $dateMore1year=date("Y-m-d", strtotime("+1 year"));// Date + 1 an
+
             $table->increments('id');
+            $table->integer('idBoss');
             $table->string('name');
             $table->string('school');            
-            $table->string('adress');
+            $table->string('address');
             $table->string('cp');
             $table->string('city');
             $table->string('uniqId');
+            $table->tinyInteger('subscription')->default('1')->comment('1 => Basic, 2 => Standard, 3 => Premium');
+            $table->date('endofsubscription')->default($dateMore1year);
             $table->timestamps();
         });
+
+             // Insertion utisateurs initiaux
+             DB::table('teams')->insert(
+                ['idBoss' => '1','name' => 'Ape de Bosdarros','school' => 'Bosdarros', 'address' => 'place de la mairie', 'cp' => '64290', 'city' => 'BOSDARROS', 'uniqId' => 'djfmlksdhj','subscription' => '3','endofsubscription' => '2100-12-31'] ); 
     }
 
     /**
